52748871d701764938089b2a17c9fc7699963d27
Njan-b <dhrishyadasph2005@gmail.com>
2026-02-20 10:09:09 +0530

    initial commit

--- src/FileLineReader.java
+++ src/FileLineReader.java
@@ -4,14 +4,23 @@ import java.io.IOException;
 
 public class FileLineReader {
     public static void main(String[] args) {
-        String fileName = "input.txt";
-        
-        // TODO: Use a try-with-resources block to open a BufferedReader for the file
-        // TODO: Read the file line by line using a loop
-        // TODO: Keep track of the line number (starting from 1)
-        // TODO: Print each line in the format "lineNumber: lineContent"
-        // TODO: Catch and handle any IOExceptions that might occur
-        
-        
+
+        try {
+            // Using relative path (file in project root)
+            BufferedReader br = new BufferedReader(new FileReader("input.txt"));
+
+            String line;
+            int lineNumber = 1;
+
+            while ((line = br.readLine()) != null) {
+                System.out.println(lineNumber + ": " + line);
+                lineNumber++;
+            }
+
+            br.close();
+
+        } catch (IOException e) {
+            System.out.println("Error reading file: " + e.getMessage());
+        }
     }
 }
